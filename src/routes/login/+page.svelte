7f9d91f888af0a6d7ce0556bdccc3bdf99d3b703
Nwol <nahom1wm@gmail.com>
2023-10-09 19:50:58 -0400

    login page and service method fixes

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { login } from '../../service/loginService.d.ts';
+	import type { LogIn } from '../../models/signup.type';
+	export const loginData: LogIn = {
+		email: '',
+		password: ''
+	};
+</script>
+
+<div class="flex flex-row">
+	<div class="basis-1/12 md:basis-1/4" />
+	<div class="card p-8 col-span-3 basis-10/12 md:basis-2/4">
+		<div class="grid grid-cols-1 gap-4">
+			<label class="label">
+				<span>Email</span>
+				<input
+                    class="input-group input-group-divider grid-cols-[auto_1fr_auto]"
+					id="email"
+					type="email"
+					placeholder="Email"
+					on:change={(e) => (loginData.email = e.currentTarget.value)}
+				/>
+			</label>
+			<label class="label">
+				<span>Password</span>
+				<input
+                    class="input-group input-group-divider grid-cols-[auto_1fr_auto]"
+					id="password"
+					type="password"
+					placeholder="Password"
+					on:change={(e) => (loginData.password = e.currentTarget.value)}
+				/>
+			</label>
+			<button
+				type="button"
+				class="btn variant-filled-surface mt-5 w-32 justify-self-end"
+				on:click={async () => await login(loginData)}
+				>Login
+			</button>
+		</div>
+	</div>
+    <div class="basis-1/12 md:basis-1/4" />
+</div>
--- src/service/loginService.d.ts
+++ src/service/loginService.d.ts
@@ -1,15 +1,16 @@
 import type { SignUp, LogIn } from "../models/signup.type.ts";
 import axios from 'axios';
+import { jwtToken } from '../lib/jwtStore.d.ts';
 
-const url = 'https://subspace-backend.fly.dev/auth/user';
+const url = 'https://subspace-backend.fly.dev';
 
 export const signup = async (signUp: SignUp) : any => {
-    const data = await axios.post(url, signUp);
-    console.log(data);
+    const data = await axios.post(`${url}/auth/user`, signUp);
+    jwtToken.set(data.data.token);
 }
 
 export const login = async (login: LogIn) : any => {
-    const data = await axios.post(url, login);
-    console.log(data);
+    const data = await axios.post(`${url}/auth/login`, login);
+    jwtToken.set(data.data.token);
 }
 
