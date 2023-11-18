ede93ee3020d7d18c3959f122ce84953cc6b8f71
Kapperchino <kamuran12580@gmail.com>
2023-11-18 17:15:51 -0500

    fixed some animations

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -6,30 +6,39 @@
 	import SideBar from '$lib/sideBar.svelte';
 	import SideDrawer from '$lib/sideDrawer.svelte';
 	import { blur } from 'svelte/transition';
-
-
 </script>
 
 <div class="drawer z-50 md:drawer-open">
-	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
-	<div class="drawer-content flex flex-col"  transition:blur>
-	  <!-- Navbar -->
-	  <div class="w-full navbar bg-base-300">
-		<div class="flex-none md:hidden">
-		  <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-		  </label>
-		</div> 
-		<div class="flex grow"></div>
-		<div class="backdrop-blur bg-base-300" >
-			<div class=" bottom-10 tabs tabs-boxed tabs-md flex">
-				<a class="tab tab-active" href="/home">Home</a>
-				<a class="tab" href="/following">Following</a>
+	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<!-- Navbar -->
+		<div class="w-full navbar bg-base-300">
+			<div class="flex-none md:hidden">
+				<label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="inline-block w-6 h-6 stroke-current"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						/></svg
+					>
+				</label>
 			</div>
+			<div class="flex grow" />
+			<div class="backdrop-blur bg-base-300">
+				<div class=" bottom-10 tabs tabs-boxed tabs-md flex">
+					<a class="tab tab-active" href="/home">Home</a>
+					<a class="tab" href="/following">Following</a>
+				</div>
+			</div>
+			<div class="flex grow" />
 		</div>
-		<div class="flex grow"></div>
-	  </div>
-	  <slot />
-	</div> 
-	<SideBar></SideBar>
-  </div>
+		<div transition:blur><slot /></div>
+	</div>
+	<SideBar />
+</div>
