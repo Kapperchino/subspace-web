046d7c0a59d23d2943084c85f55e3622570f5b22
Kapperchino <kamuran12580@gmail.com>
2023-11-19 00:44:38 -0500

    fix

--- src/lib/postCardComponent.svelte
+++ src/lib/postCardComponent.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 
-
 	import VoteComponent from '$lib/voteComponent.svelte';
 	import { VoteType, type PictureMeta, type Post, type VideoMeta } from '../models/post.type';
 	import PostCardMetaComponent from './postCardMetaComponent.svelte';
 	import TimeComponent from './timeComponent.svelte';
-
 
 	export let post: Post | undefined;
 	export let spaceId: number | undefined;
@@ -35,70 +33,69 @@
 		return url;
 	}
 
-	function clickTo(){
+	function clickTo() {
 		goto(`/s/${post?.space_parent_id}/${post?.space_id}/p/${post?.id}`);
 	}
 </script>
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<div class="card card-compact shadow-lg bg-base-100 hover:cursor-pointer" on:click={clickTo}>
-		<div class="card-body">
-			<div class="flex-row flex">
-				<div ><PostCardMetaComponent {post} {spaceId} /></div>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="card card-compact shadow-lg bg-base-100 hover:cursor-pointer" on:click={clickTo}>
+	<div class="card-body">
+		<div class="flex-row flex">
+			<div><PostCardMetaComponent {post} {spaceId} /></div>
+		</div>
+
+		{#if post?.topic != ''}
+			<a class="card-title" href="/s/{post?.space_parent_id}/{post?.space_id}/p/{post?.id}">
+				<h3>
+					{post?.topic}
+				</h3>
+			</a>
+		{/if}
+		{#if post?.body != ''}
+			<a href="/s/{post?.space_parent_id}/{post?.space_id}/p/{post?.id}">
+				<section class="p-2 break-words subpixel-antialiased">
+					{post?.body}
+				</section>
+			</a>
+		{/if}
+		{#if post?.post_pictures != null}
+			<div class="flex justify-center">
+				<div class="basis-1/4 rounded-md bg-gradient-to-r from-gray-900 to-gray-800" />
+				<img
+					class="rounded-md"
+					alt="The project logo"
+					height={getDimention(post?.post_pictures?.at(0)).height}
+					width={getDimention(post?.post_pictures?.at(0)).width}
+					src={post?.post_pictures?.at(0)?.url}
+				/>
+
+				<div class="basis-1/4 rounded-md bg-gradient-to-l from-gray-900 to-gray-800" />
 			</div>
-	
-			{#if post?.topic != ''}
-				<a class="card-title" href="/s/{post?.space_parent_id}/{post?.space_id}/p/{post?.id}">
-					<h3>
-						{post?.topic}
-					</h3>
-				</a>
-			{/if}
-			{#if post?.body != ''}
-				<a href="/s/{post?.space_parent_id}/{post?.space_id}/p/{post?.id}">
-					<section class="p-2">{post?.body}</section>
-				</a>
-			{/if}
-			{#if post?.post_pictures != null}
-				<div
-					class="flex justify-center"
-				>
-					<div class="basis-1/4 rounded-md bg-gradient-to-r from-gray-900 to-gray-800" />
-					<img
-						class="rounded-md"
-						alt="The project logo"
-						height={getDimention(post?.post_pictures?.at(0)).height}
-						width={getDimention(post?.post_pictures?.at(0)).width}
-						src={post?.post_pictures?.at(0)?.url}
-					/>
-	
-					<div class="basis-1/4 rounded-md bg-gradient-to-l from-gray-900 to-gray-800" />
-				</div>
-			{/if}
-			{#if post?.post_videos != null}
-				
-				<div style="position: relative; padding-top: 80%;">
-					<!-- svelte-ignore a11y-missing-attribute -->
-					<iframe
-						src={getVideoUrl(post.post_videos[0])}
-						style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
-						allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
-						allowfullscreen={true}
-					/>
-				</div>
-			{/if}
-			<div class="card-actions">
-				<div class="pt-2"><TimeComponent time={post?.created} /></div>
-				<div class="grow" />
-				<div class="z-10">
-					<VoteComponent
-						upVotes={post?.up_votes}
-						downVotes={post?.down_votes}
-						voteData={post?.vote}
-						id={post?.id}
-						voteType={VoteType.Post}
-					/>
-				</div>
+		{/if}
+		{#if post?.post_videos != null}
+			<div style="position: relative; padding-top: 80%;">
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<iframe
+					src={getVideoUrl(post.post_videos[0])}
+					style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;"
+					allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+					allowfullscreen={true}
+				/>
+			</div>
+		{/if}
+		<div class="card-actions">
+			<div class="pt-2"><TimeComponent time={post?.created} /></div>
+			<div class="grow" />
+			<div class="z-10">
+				<VoteComponent
+					upVotes={post?.up_votes}
+					downVotes={post?.down_votes}
+					voteData={post?.vote}
+					id={post?.id}
+					voteType={VoteType.Post}
+				/>
 			</div>
 		</div>
 	</div>
-
+</div>
