441e8b2b533224275ddd3fb9d740b60b33019d05
Kapperchino <kamuran12580@gmail.com>
2023-11-22 22:06:44 -0500

    change

--- src/lib/sideBar.svelte
+++ src/lib/sideBar.svelte
@@ -4,7 +4,11 @@
 	import Satellite from '~icons/material-symbols/satellite-alt-outline-sharp';
 	import Bell from '~icons/material-symbols/notifications';
 	import PostingComponent from './postingComponent.svelte';
-	import { invalidateAll } from '$app/navigation';
+	import { goto, invalidateAll } from '$app/navigation';
+	import { coockieStore } from './store/tokenStore';
+	import { onMount } from 'svelte';
+	import type { UserMeta } from '../models/signup.type';
+	import AvatarComponent from './avatarComponent.svelte';
 	let modal: HTMLDialogElement | undefined;
 
 	function startPost() {
@@ -15,6 +19,15 @@
 		await invalidateAll();
 		modal?.close();
 	};
+
+	async function onClick() {
+		await goto(`/users/${user.user_id}`);
+	}
+
+	let user: UserMeta;
+	onMount(() => {
+		user = coockieStore.getValue('cookie');
+	});
 </script>
 
 <div class="drawer-side">
@@ -67,13 +80,31 @@
 		<div class="btn btn-primary btn-sm h-12 mt-3" on:click={startPost}>
 			<div class="text-lg flex flex-row">Post</div>
 		</div>
+		<div class="flex flex-col mt-auto hover:cursor-pointer" on:click={onClick} >
+			<div class="grow" />
+			<div class="card card-compact bg-base-300">
+				<div class="card-body">
+					<div class="flex-row flex">
+						<div class="avatar">
+							<div class="w-10 rounded-full">
+								<AvatarComponent url={user?.picture_meta?.url} userId={user?.user_id} />
+							</div>
+						</div>
+						<div class="pl-2">
+							<div class="pl-1 font-semibold">{user?.display_name}</div>
+							<div class="pl-0.5 text-primary">@{user?.user_address}</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</ul>
 </div>
 
 <dialog id="my_modal_2" class="modal" bind:this={modal}>
 	<div class="modal-box bg-primary-content">
 		<div class="grow">
-			<PostingComponent onSuccess={onSuccess} />
+			<PostingComponent {onSuccess} />
 		</div>
 	</div>
 	<form method="dialog" class="modal-backdrop">
