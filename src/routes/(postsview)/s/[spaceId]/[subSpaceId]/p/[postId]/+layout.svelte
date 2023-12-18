282a74369df1ecd9699d47895ee2f5fcd29ac969
Kapperchino <kamuran12580@gmail.com>
2023-12-17 20:55:30 -0500

    added back button

--- src/routes/(postsview)/s/[spaceId]/[subSpaceId]/p/[postId]/+layout.svelte
+++ src/routes/(postsview)/s/[spaceId]/[subSpaceId]/p/[postId]/+layout.svelte
@@ -7,6 +7,9 @@
 	import type { UserMeta } from '../../../../../../../models/signup.type';
 
 	export let data: import('./$types').LayoutData;
+
+	import Back from '~icons/ic/baseline-arrow-back';
+
 	let user: UserMeta;
 	onMount(() => {
 		user = coockieStore.getValue('cookie');
@@ -15,13 +18,14 @@
 
 <ShellComponent>
 	<svelte:fragment slot="navbar">
-		<a class="flex sm:hidden" href="/users/{user?.user_id}">
-			<div class="avatar">
-				<div class="w-10 rounded-full">
-					<AvatarComponent url={user?.picture_meta?.url} userId={user?.user_id} />
-				</div>
-			</div>
-		</a>
+		<button
+			type="button"
+			on:click={() => {
+				history.back();
+			}}
+		>
+			<div class="text-lg font-bold"><Back /></div>
+		</button>
 		<div class="flex-1" />
 		<div class="md:flex-1" />
 		<div class="flex backdrop-blur bg-base-300/60 ml-3 rounded-lg">
